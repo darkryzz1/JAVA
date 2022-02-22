b1c20d09b4d94fedcfc63988d1dfea12a789210d
darkryzz1 <82153601+darkryzz1@users.noreply.github.com>
2022-02-22 19:43:51 +0600

    Update Course.java

--- src/Course.java
+++ src/Course.java
@@ -5,8 +5,7 @@ public class Course
     private int credit;
     private String grade;
     private double attendance;
-    /** No-arg constructor to make an instance of class.
-     */
+
     public Course()
     {
         name = "";
@@ -14,12 +13,6 @@ public class Course
         grade = "";
         attendance = 0;
     }
-
-    /** Parameterized constructor to make an instance of class.
-     * @param name A String representing the name of a course.
-     * @param credit An integer representing the credit of a course.
-     * @param grade A String representing the grade of a course.
-     */
     public Course(String name, int credit, String grade)
     {
         this.name = name;
@@ -28,44 +21,26 @@ public class Course
         this.attendance = attendance;
     }
 
-    /** Gets the name of a course.
-     * @return A String representing the name of a course.
-     */
     public String getName() {
         return name;
     }
 
-    /** Sets the name of a course.
-     * @param name A String representing the name of a course.
-     */
     public void setName(String name) {
         this.name = name;
     }
 
-    /** Gets the credit of a course.
-     * @return An integer representing the credit of a course.
-     */
     public double getCredit() {
         return credit;
     }
 
-    /** Sets the credit of a course.
-     * @param credit An integer representing the credit of a course.
-     */
     public void setCredit(int credit) {
         this.credit = credit;
     }
 
-    /** Gets the grade of a course.
-     * @return A String representing the grade of a course.
-     */
     public String getGrade() {
         return grade;
     }
 
-    /** Sets the grade of a course.
-     * @param grade A String representing the grade of a course.
-     */
     public void setGrade(String grade) {
         this.grade = grade;
     }
